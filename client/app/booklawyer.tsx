8a4bb7096c4888a058c3a41b84665e1b730467a7
Joaquin Miguel Angeles <joaquinmiguel.ja@gmail.com>
2025-11-20 16:04:50 +0800

    fix: lawyer consult page layout

--- client/app/booklawyer.tsx
+++ client/app/booklawyer.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 import BookLawyer from "../components/booklawyer/LawyerBookingView";
+import Colors from "../constants/Colors";
 
 export default function BookLawyerPage() {
-  return <BookLawyer />;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background.primary }} edges={["top", "left", "right"]}>
+      <StatusBar barStyle="dark-content" backgroundColor={Colors.background.primary} />
+      <BookLawyer />
+    </SafeAreaView>
+  );
 }
