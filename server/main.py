99d64923c10a1737812d4a658f7cf92f18ea7732
Joaquin Miguel Angeles <joaquinmiguel.ja@gmail.com>
2025-11-21 19:37:28 +0800

    fix: railway config

--- server/main.py
+++ server/main.py
@@ -149,7 +149,7 @@ if os.getenv("NODE_ENV") == "production":
 
 app.add_middleware(
     TrustedHostMiddleware, 
-    allowed_hosts=["*"] if os.getenv("NODE_ENV") == "development" else os.getenv("ALLOWED_HOSTS", "*.railway.app,localhost,127.0.0.1").split(",")
+    allowed_hosts=["*"] if os.getenv("NODE_ENV") == "development" else os.getenv("ALLOWED_HOSTS", "*.railway.app,localhost,127.0.0.1,10.0.2.2").split(",")
 )
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
